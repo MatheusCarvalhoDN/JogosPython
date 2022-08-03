c699cace87c1768fe8e861c36763d1990eb20715
MATHEUS <estagiarioti@grupomb.local>
2022-08-03 15:04:57 -0300

    Primeiro commit

---
+++ jogos.py
@@ -0,0 +1,22 @@
+import forca
+import advinhacao
+
+print("******************")
+print("Escolha o seu jogo")
+print("******************")
+
+jogo = int(input("Qual jogo quer executar ? (1) Forca (2) Advinhação: "))
+
+if (jogo == 1):
+    print("Jogando Forca")
+    forca.jogar()
+elif (jogo == 2):
+    print("Jogando Advinhação")
+    advinhacao.jogar()
+
+
+
+
+
+
+
